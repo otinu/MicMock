b7f1618094cfc3f5bf0c2d33f69c2a07c9b76e7b
otinu <fitshter@gmail.com>
2024-10-25 23:00:57 +0900

    Initial Commit

---
+++ MicMock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MicMock
+//
+//  Created by Apple on 2024/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MicMock/MicMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicMockApp.swift
+//  MicMock
+//
+//  Created by Apple on 2024/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MicMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
